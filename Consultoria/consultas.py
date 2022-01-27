822a1050329011bfd073c9d25702a0ad685cc66a
Zaply0002 <cristianzaplana130@gmail.com>
2022-01-27 20:38:02 +0100

    01.27

--- Consultoria/consultas.py
+++ Consultoria/consultas.py
@@ -1,23 +1,46 @@
 
 import ifcopenshell as ifc
+import ifcopenshell.util.element as element
 
 
 
+def cargarArchivo(ruta):
+    global archivo
+    archivo = ifc.open(ruta)
 
-def obtenerProyecto(rutaArchivo):
-    proyecto = NULL
-    try:
-        proyecto = archivoIfc.by_type("IfcProyect")[0]
-    except:
-        print("No se pudo obtener el proyecto")
-    return proyecto
+def obtenerObjeto(tipo, posicion=0):
+   return archivo.by_type(tipo)[posicion]
+   
+def ListaIfcObject(tipo):
+    return archivo.by_type(tipo)
 
-
-def ObtenerInfoOnjetos(archivoIfc,tipo = "IfcBuildingElement"):
-    elementos = archivoIfc.by_type(tipo)
+def ObtenerInfoOnjetos(tipo = "IfcBuildingElement"):
+    
+    elementos = archivo.by_type(tipo)
     for elemento in elementos:
         print(elemento)
     print('Hay {} elementos de tipo {} en la construcción'.format(len(elementos),tipo))
 
-archivo = ifc.open('d')
-archivo
+
+def ObtenerNumeroXTipo(tipo):
+    elementos = archivo.by_type(tipo)
+    return len(elementos)
+
+def ListaIfcObject(tipo):
+    return archivo.by_type(tipo)
+
+def ObtenerPropiedadesObjeto(IfcObject):
+    return element.get_psets(IfcObject)
+
+def CadenaPropiedadesFiltradoObjeto(objeto, tipo):
+    propiedades = element.get_psets(objeto)
+    filtrados= propiedades.get(tipo)
+    print(filtrados)
+
+def TipoDePropiedadesObjeto(objeto):
+    lista = element.get_psets(objeto).keys()
+    claves = []
+    for i in lista:
+        claves.append(i)
+    return claves
+
--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 
-import archivo as con
+import Consultoria.consultas as con
+archivo = "Proyecto1_Normal.ifc"
+con.cargarArchivo(archivo)
 
-con.abrirArchivo('prueba02.ifc')
-
-con.ObtenerInfoOnjetos()
+obj = con.ListaIfcObject('IfcSlab')
 
 
+x = con.ObtenerPropiedadesObjeto(obj[0])
+#print (x)
+
+con.CadenaPropiedadesFiltradoObjeto(obj[0],'BaseQuantities')
+
+y = con.TipoDePropiedadesObjeto(obj[0])
+
+print(y)
